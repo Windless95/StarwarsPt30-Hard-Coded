06b8ab42a01b644c600b5991b2e6f634673defee
Windless95 <barrerajoelj@gmail.com>
2022-01-20 01:35:24 +0000

    finished hardcoding

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,40 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { PlanetCards } from "../component/Planetcards";
+import { CharacterCards } from "../component/CharacterCards";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+
+	return (
+		<div className="container-fluid p-0 pb-4 m-0 bg-dark bg-opacity-75">
+			<h1 className="mx-0 px-4 pb-2 pt-4 text-white">Characters</h1>
+			<div className="d-flex flex-row mx-auto" style={{ width: "90%", overflow: "auto" }}>
+				<CharacterCards/>
+				<CharacterCards/>
+				<CharacterCards/>
+				<CharacterCards/>
+				<CharacterCards/>
+				<CharacterCards/>
+				<CharacterCards/>
+				<CharacterCards/>
+				
+			</div>
+			<h1 className="mx-0 px-4 pb-2 pt-4 text-white">Planets</h1>
+			<div className="d-flex flex-row mx-auto mb-4" style={{ width: "90%", overflow: "auto" }}>
+				<PlanetCards/>
+				<PlanetCards/>
+				<PlanetCards/>
+				<PlanetCards/>
+				<PlanetCards/>
+				<PlanetCards/>
+				<PlanetCards/>
+				<PlanetCards/>
+				<PlanetCards/>
+				<PlanetCards/>
+				<PlanetCards/>
+				<PlanetCards/>
+				
+			</div>
+		</div>
+	);
+};
