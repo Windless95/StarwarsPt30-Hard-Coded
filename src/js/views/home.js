6da4de9b603c978b11781687a31f696ed7971995
Windless95 <barrerajoelj@gmail.com>
2022-01-25 02:01:58 +0000

    almost finished need to add navbar

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,39 +1,24 @@
-import React from "react";
-import "../../styles/home.css";
-import { PlanetCards } from "../component/Planetcards";
-import { CharacterCards } from "../component/CharacterCards";
+import React, { useContext } from "react";
+import { CharacterCard } from "../component/CharacterCard";
+import { PlanetCard } from "../component/PlanetCard";
+import { myContext } from "../store/appContext";
 
 export const Home = () => {
+	const { store } = useContext(myContext);
 
 	return (
 		<div className="container-fluid p-0 pb-4 m-0 bg-dark bg-opacity-75">
 			<h1 className="mx-0 px-4 pb-2 pt-4 text-white">Characters</h1>
 			<div className="d-flex flex-row mx-auto" style={{ width: "90%", overflow: "auto" }}>
-				<CharacterCards/>
-				<CharacterCards/>
-				<CharacterCards/>
-				<CharacterCards/>
-				<CharacterCards/>
-				<CharacterCards/>
-				<CharacterCards/>
-				<CharacterCards/>
-				
+				{store.characterList.map((character, index) => (
+					<CharacterCard key={index} card_character={character} index={index} />
+				))}
 			</div>
 			<h1 className="mx-0 px-4 pb-2 pt-4 text-white">Planets</h1>
 			<div className="d-flex flex-row mx-auto mb-4" style={{ width: "90%", overflow: "auto" }}>
-				<PlanetCards/>
-				<PlanetCards/>
-				<PlanetCards/>
-				<PlanetCards/>
-				<PlanetCards/>
-				<PlanetCards/>
-				<PlanetCards/>
-				<PlanetCards/>
-				<PlanetCards/>
-				<PlanetCards/>
-				<PlanetCards/>
-				<PlanetCards/>
-				
+				{store.planetList.map((planet, index) => (
+					<PlanetCard key={index} card_planet={planet} index={index} />
+				))}
 			</div>
 		</div>
 	);
